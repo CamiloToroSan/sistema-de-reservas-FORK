95090bd6099f7033c4ec8d1904ef164ecbdd7824
Camilo Toro <camilotoro@email.edu.co>
2026-08-25 09:43:16 -0500

    Borrando firma y comentarios del profe daniel en unos archivos

--- src/main/java/com/mycompany/sistema/reservas/dominio/modelo/Reserva.java
+++ src/main/java/com/mycompany/sistema/reservas/dominio/modelo/Reserva.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sistema.reservas.dominio.modelo;
 
 import java.util.UUID;
 
-/**
- *
- * @author daferarte
- */
 public class Reserva {
     private final UUID id;            // Inmutable 
     private final Cliente cliente;    // Asociación directa con la entidad Cliente
